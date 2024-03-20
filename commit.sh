d72a1fc93e4a5a11eca429185bfbc377b557e3d4
ShragSir <162401234+ShragSir@users.noreply.github.com>
2024-03-20 02:17:47 +0000

    Changes to be committed: 	modified:   commit.sh

--- commit.sh
+++ commit.sh
@@ -2,17 +2,22 @@ echo "Would you like to leave a comment?
             [Y/N]  "
 read message
 
-if [ y ]
+if [ $message =  y ]
 then
 
+ clear
+ echo "Enter comment:"
+ read message
  git add .
  git commit -m "$message"
  git push
 
 else
 
+ break
+
+fi
+
 git add .
 git commit
 git push
-
-fi
